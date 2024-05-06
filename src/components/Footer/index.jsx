00a86b62fc93c62775b07fa96ed67c0a793017a4
OlozanoTamy <Olozanodeveloper@gmail.com>
2024-05-06 14:05:39 -0500

    First Commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+const date = new Date().getFullYear();
+function Footer() {
+  return (
+    <>
+      <footer>
+        <p>CopyRight ©️ {date}</p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
